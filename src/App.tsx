88339429aa1907238d652b9f3905b736e2a6f5be
zhuyouwei <zhu.youwei@gmail.com>
2020-04-20 02:15:51 +0800

    posenet demo with webcam

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,95 @@
 import * as React from "react";
 import "./styles.css";
+import {useEffect, useRef, useState} from "react";
+import {
+    drawBoundingBox,
+    drawKeypoints,
+    drawSkeleton,
+    isMobile,
+    toggleLoadingUI,
+    tryResNetButtonName,
+    tryResNetButtonText,
+    updateTryResNetButtonDatGuiCss
+} from './util.js';
+import {multiPoseDetection, usePoseNet} from "./hook"
+import {Pose} from "@tensorflow-models/posenet";
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+    const [start, setStart] = useState<boolean>(false);
+
+    const video = useRef<HTMLVideoElement>(null);
+    const poses = usePoseNet(video.current!);
+    const canvas = useRef<HTMLCanvasElement>(null);
+
+    draw(canvas.current, video.current, poses);
+
+    useEffect(() => {
+        (async () => {
+            await setupCamera(video.current);
+        })();
+    }, []);
+
+    return (
+        <>
+            <h1>Body Pose Demo</h1>
+            <button onClick={(e) => setStart(true)}>Start</button>
+            <video
+                ref={video}
+                width="640"
+                height="480"
+                style={{display:"none"}}
+                autoPlay
+            >
+            </video>
+            <canvas ref={canvas} width="640" height="480"/>
+        </>
+    );
+}
+const videoWidth = 640;
+const videoHeight = 480;
+function draw(canvas: HTMLCanvasElement | null, video: HTMLVideoElement | null, poses: Pose []) {
+    console.log(poses)
+    if (canvas === null || video === null) {
+        return;
+    }
+    const ctx = canvas.getContext('2d');
+    if (ctx === null){
+        return
+    }
+    ctx.save();
+    ctx.scale(-1, 1);
+    ctx.translate(-videoWidth, 0);
+    ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
+    ctx.restore();
+    poses.forEach(({score, keypoints}) => {
+        if (score >= multiPoseDetection.minPoseConfidence) {
+            drawKeypoints(keypoints, multiPoseDetection.minPartConfidence, ctx);
+            drawSkeleton(keypoints, multiPoseDetection.minPartConfidence, ctx);
+            //drawBoundingBox(keypoints, ctx);
+        }
+    });
+}
+
+/**
+ * Loads a the camera to be used in the demo
+ *
+ */
+async function setupCamera(video: any) {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+        throw new Error(
+            'Browser API navigator.mediaDevices.getUserMedia not available');
+    }
+    video.width = videoWidth;
+    video.height = videoHeight;
+    navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {
+            facingMode: 'user',
+            width: videoWidth,
+            height: videoHeight
+        }
+    }).then(function (stream) {
+        video.srcObject = stream;
+        video.play();
+    });
 }
---
+++ src/hook.ts
@@ -0,0 +1,53 @@
+import {PosenetInput} from "@tensorflow-models/posenet/dist/types";
+import {useEffect, useRef, useState} from "react";
+import * as posenet from '@tensorflow-models/posenet';
+import {Pose, PoseNet, ModelConfig} from "@tensorflow-models/posenet";
+import {isMobile} from "./util";
+
+
+const config = {
+    architecture: 'MobileNetV1',
+    outputStride: 16,
+    inputResolution: {width: 640, height: 480},
+    multiplier: isMobile() ? 0.50 : 0.75,
+    quantBytes: 2
+}
+export const multiPoseDetection = {
+    maxPoseDetections: 5,
+    minPoseConfidence: 0.15,
+    minPartConfidence: 0.1,
+    nmsRadius: 30.0,
+};
+
+export function usePoseNet(input: PosenetInput) {
+    const [poses, setPoses] = useState<Pose[]>([]);
+    const net = useRef<PoseNet>();
+
+    useEffect(() => {
+        (async () => {
+            if (input === null) {
+                return [];
+            }
+            net.current = await posenet.load({...config} as ModelConfig);
+
+            if ("autoplay" in input) {
+                //render(input, net.current, setPoses);
+                window.requestAnimationFrame(render(input, net.current, setPoses));
+            }
+        })();
+    }, [input]);
+    return poses;
+}
+
+const render = (input: PosenetInput, net: PoseNet, setPoses: any) => async () => {
+    const res: Pose [] = await net.estimatePoses(input, {
+        flipHorizontal: true,
+        decodingMethod: 'multi-person',
+        maxDetections: multiPoseDetection.maxPoseDetections,
+        scoreThreshold: multiPoseDetection.minPartConfidence,
+        nmsRadius: multiPoseDetection.nmsRadius
+    });
+    setPoses(res);
+    //setInterval(render(input, net, setPoses),500);
+    window.requestAnimationFrame(render(input, net, setPoses));
+};
